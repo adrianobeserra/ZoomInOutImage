cfa408c3129a3da32f50efc6179b59bf2a749d0c
Carlos Adriano <adrianobeserra@gmail.com>
2018-08-22 22:51:57 -0300

    Algoritmo Ajustado

--- src/pdi/br/poli/ecomp/ConfigImage.java
+++ src/pdi/br/poli/ecomp/ConfigImage.java
@@ -2,8 +2,6 @@ package pdi.br.poli.ecomp;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,40 +12,38 @@ public class ConfigImage {
     private int atualHeight;
     private int origWidth;
     private int origHeight;
-    private int n = 5;
 
 
-    public ConfigImage(String name, int atualWidth, int atualHeight, int origWidth, int origHeight, int n) {
+    public ConfigImage(String name, int atualWidth, int atualHeight, int origWidth, int origHeight) {
         this.name = name;
         this.atualWidth = atualWidth;
         this.atualHeight = atualHeight;
         this.origWidth = origWidth;
         this.origHeight = origHeight;
-        this.n = n;
     }
 
     public void zoonIn() {
-
-        int w = n * atualWidth;
-        int h = n * atualHeight;
-        File processedDir = new File("processed");
-
-        if (!processedDir.exists()) {
-            processedDir.mkdir();
-        }
-        BufferedImage atualImage = new
-                BufferedImage(atualWidth, atualHeight, BufferedImage.TYPE_BYTE_GRAY);
-        BufferedImage originalImage = new BufferedImage(w, h, atualImage.getType());
-
-
-        for (int y=0; y < h; ++y)
-            for (int x=0; x < w; ++x)
-                originalImage.setRGB(x, y, atualImage.getRGB(x/n, y/n));
-
-
-
         try {
-            ImageIO.read(new File(name));
+            int n = origWidth / atualWidth;
+            int w = origWidth;
+            int h = origHeight;
+            File processedDir = new File("processed");
+
+            if (!processedDir.exists()) {
+                processedDir.mkdir();
+            }
+            BufferedImage atualImage = ImageIO.read(new File(name));
+            BufferedImage originalImage = new BufferedImage(w, h, atualImage.getType());
+            Graphics2D g = originalImage.createGraphics();
+            g.setColor(Color.WHITE);
+            g.fillRect(0, 0, originalImage.getWidth(), originalImage.getHeight());
+            g.dispose();
+
+            for (int y=0; y < h; ++y)
+                for (int x=0; x < w; ++x)
+                    originalImage.setRGB(x, y, atualImage.getRGB(x/n, y/n));
+
+
             ImageIO.write(originalImage, "jpg", new File("processed//".concat(name)));
             System.out.println(name + " was processed.");
         } catch (IOException e) {
--- src/pdi/br/poli/ecomp/Main.java
+++ src/pdi/br/poli/ecomp/Main.java
@@ -11,9 +11,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        ConfigImage Zoom_in_1 = new ConfigImage("Zoom_in_(1).jpg", 100,150,360,480, 2);
-        ConfigImage Zoom_in_2 = new ConfigImage("Zoom_in_(2).jpg", 890,500,2592,1456, 2 );
-        ConfigImage Zoom_in_3 = new ConfigImage("Zoom_in_(3).jpg", 250,330,720,990, 2 );
+        ConfigImage Zoom_in_1 = new ConfigImage("Zoom_in_(1).jpg", 100,150,360,480);
+        ConfigImage Zoom_in_2 = new ConfigImage("Zoom_in_(2).jpg", 890,500,2592,1456);
+        ConfigImage Zoom_in_3 = new ConfigImage("Zoom_in_(3).jpg", 250,330,720,990);
         List<ConfigImage> imagesToZoomIn = new ArrayList<>();
         imagesToZoomIn.add(Zoom_in_1);
         imagesToZoomIn.add(Zoom_in_2);
@@ -22,9 +22,9 @@ public class Main {
             image.zoonIn();
         }
 
-        ConfigImage Zoom_out_1 = new ConfigImage("Zoom_out_(1).jpg", 750,500,271,120, 2 );
-        ConfigImage Zoom_out_2 = new ConfigImage("Zoom_out_(2).jpg", 990,1600,317,500 , 2);
-        ConfigImage Zoom_out_3 = new ConfigImage("Zoom_out_(3).jpg", 330,750,174,500 , 2);
+        ConfigImage Zoom_out_1 = new ConfigImage("Zoom_out_(1).jpg", 750,500,271,120);
+        ConfigImage Zoom_out_2 = new ConfigImage("Zoom_out_(2).jpg", 990,1600,317,500);
+        ConfigImage Zoom_out_3 = new ConfigImage("Zoom_out_(3).jpg", 330,750,174,500);
         List<ConfigImage> imagesToZoomOut = new ArrayList<>();
         imagesToZoomOut.add(Zoom_out_1);
         imagesToZoomOut.add(Zoom_out_2);
